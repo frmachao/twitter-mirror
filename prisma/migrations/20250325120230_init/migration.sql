d1381f1cbd3c3b253412f260d7af479529faf695
machao <frmachao@126.com>
2025-03-25 20:31:38 +0800

    feat(database): migrate from SQLite to PostgreSQL

--- prisma/migrations/20250325120230_init/migration.sql
+++ prisma/migrations/20250325120230_init/migration.sql
@@ -1,14 +1,16 @@
 -- CreateTable
 CREATE TABLE "ProcessState" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "startTime" BIGINT NOT NULL,
     "updatedAt" BIGINT NOT NULL,
-    "lastTweetId" TEXT
+    "lastTweetId" TEXT,
+
+    CONSTRAINT "ProcessState_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Tweet" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "authorId" TEXT NOT NULL,
     "conversationId" TEXT,
     "inReplyToUserId" TEXT,
@@ -19,14 +21,20 @@ CREATE TABLE "Tweet" (
     "isRoot" BOOLEAN NOT NULL DEFAULT false,
     "threadId" TEXT,
     "translatedText" TEXT,
-    CONSTRAINT "Tweet_threadId_fkey" FOREIGN KEY ("threadId") REFERENCES "Thread" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+    CONSTRAINT "Tweet_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Thread" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "authorId" TEXT NOT NULL,
     "createdAt" BIGINT NOT NULL,
     "status" TEXT NOT NULL DEFAULT 'pending',
-    "updatedAt" BIGINT
+    "updatedAt" BIGINT,
+
+    CONSTRAINT "Thread_pkey" PRIMARY KEY ("id")
 );
+
+-- AddForeignKey
+ALTER TABLE "Tweet" ADD CONSTRAINT "Tweet_threadId_fkey" FOREIGN KEY ("threadId") REFERENCES "Thread"("id") ON DELETE SET NULL ON UPDATE CASCADE;
